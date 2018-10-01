96e928a1714a0a57b64a1cce073cf5401cd1031d
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-10-01 11:14:20 -0700

    add project 2 files

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,161 @@
+/* Project 2 main
+ *
+ *    Do not submit this file
+ *
+ */
+
+#include<iostream>
+#include<cstring>
+#include<assert.h>
+using std::cout;
+using std::endl;
+
+using std::strcat;
+using std::strcmp;
+using std::strcpy;
+using std::strlen;
+
+
+
+/* Main functions
+ *
+ *    you may make any changes you want to main to test your functions.
+ *    We will be using our own to grade.
+ *
+ *    I'm using preprocessor directives to layout different mains to test
+ *    various functions.  I have 6 example tests.  You can add or replace,
+ *    these are here just to illustrate the technique. Cleaner than
+ *    commenting/uncommenting blocks of code.
+ *
+ */
+
+// Choose which test to compile
+#define TEST0 //TEST1 TEST2 TEST3 TEST4
+
+
+
+#ifdef TEST0
+
+// Uncomment this to make the WordList member variables public
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+int main() {
+    
+    WordList *wordlist = new WordList(5);
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
+
+#elif defined TEST1  //Test constructor
+
+
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+int main() {
+    
+    WordList *wordlist = new WordList(5);
+    
+    // Assuming we made our member variables public:
+    cout << "Count: " << wordlist->m_count << endl; // Expect 0
+    
+    cout << "Capacity: " << wordlist->m_max_words << endl; // Expect 5
+    
+    
+    return 0;
+}
+#elif defined TEST2
+
+
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+// Same as above but using asserts instead of cout
+//
+// asserts are a way to check conditions, they will gracefully crash
+// the program if the condition is not met.
+//
+// This assumes you know what the end state after the function call
+// should be.  Effective programmers have in mind test cases prior to
+// implmentation.  You should know what the end state should be after
+// any function call.
+int main() {
+    WordList *wordlist = new WordList(5);
+    
+    // Assuming we made our member variables public:
+    // If we failed to set our member variables correctly
+    // These assertions will fail
+    assert(wordlist->m_count == 0);
+    assert(wordlist->m_max_words == 5);
+    
+    
+    return 0;
+}
+#elif defined TEST3
+
+
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+// Test addWord: Both Constructor and addWord must be
+// implmented prior to running this code, otherwise
+// it will crash.
+int main() {
+    WordList *wordlist = new WordList(5);
+    wordlist->addWord("harry");
+    wordlist->addWord("ron");
+    wordlist->addWord("hermione");
+    
+    // Assuming we made our member variables public:
+    cout  << wordlist->m_list[0][2] << endl; // Expect 'r'
+    
+    cout  << wordlist->m_list[1] << endl; // Expect "ron"
+    
+    return 0;
+}
+
+#elif defined TEST4
+
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+// Test addWord: same as TEST3 but with asserts. Both Constructor and addWord
+// must be implmented prior to running this code, otherwise it will crash.
+int main() {
+    
+    WordList *wordlist = new WordList(5);
+    wordlist->addWord("harry");
+    wordlist->addWord("ron");
+    wordlist->addWord("hermione");
+    
+    // Assuming we made our member variables public:
+    assert(wordlist->m_list[0][2] == 'r' ); // Individual character, can use ==
+    
+    assert(strcmp(wordlist->m_list[1], "ron") == 0); // cstring, must use strcmp
+    
+    return 0;
+}
+#else
+
+
+//#define MAKE_MEMBERS_PUBLIC
+#include"wordlist.h"
+
+// Test: printList(): Constructor, addWord, printList must be implmented prior to
+// running this code, otherwise it will crash.
+int main() {
+    
+    WordList *wordlist = new WordList(5);
+    wordlist->addWord("harry");
+    wordlist->addWord("ron");
+    wordlist->addWord("hermione");
+    
+    wordlist->printList();
+    
+    return 0;
+}
+
+#endif
---
+++ Project2/Project2/wordlist.cpp
@@ -0,0 +1,194 @@
+/* WordList source file
+ *
+ *
+ *    This file will contain the function definitions you will implement.
+ *    The function signitures may NOT be changed.  You may create your own
+ *    helper functions and include them in this file.
+ *
+ *    In addition to the specific instructions for each function, no function
+ *    should cause any memory leaks or alias m_list in any way that would result
+ *    in undefined behavior.
+ *
+ *    Topics: Multilevel Arrays, Pointers, Dynamic Allocation, Classes
+ *
+ */
+
+
+// Don't not include any other libraries
+#include"wordlist.h"
+#include<iostream>
+#include<cstring>
+
+
+using std::cout;
+using std::endl;
+
+using std::strcat;
+using std::strcmp;
+using std::strcpy;
+using std::strlen;
+
+
+/* Function: Wordlist Constructor
+ *
+ *    Constructs an empty Wordlist that is allocated enough space to store
+ *    max_words many words.  If max_words is less than 1, set m_list to nullptr
+ *    and the other member variables should be made consitent with this state.
+ */
+WordList::WordList(const int max_words) {
+    
+    // TODO:
+    
+}
+
+
+/* Function: Wordlist Copy Constructor
+ *
+ *    Constructs a Wordlist that is a copy of an existing WordList
+ */
+WordList::WordList(const WordList &other) {
+    
+    // TODO:
+    
+}
+
+
+/* Function: Wordlist Destructor
+ *
+ *    Cleans up WordList's dynamically allocated memory.
+ */
+WordList::~WordList() {
+    
+    // TODO:
+    
+}
+
+/* Function: printList
+ *
+ *    Prints all the words in WordList in a single line with spaces between each
+ *    word, then followed by a newline after the last word. Returns the number
+ *    of words printed.  If m_list is nullptr there is nothing to print, return -1.
+ */
+int    WordList::printList() const {
+    
+    // TODO:
+    return -1;
+    
+}
+
+
+
+/* Function: getAt
+ *
+ *    Returns the word at the index position in the WordList.
+ *    return nullptr if the index is out of bounds.
+ */
+char* WordList::getAt(const int index) const {
+    
+    // TODO:
+    return nullptr;
+    
+}
+
+
+/* Function: getCount
+ *
+ *    Returns the number of words currently stored in WordList.
+ */
+int    WordList::getCount() const {
+    
+    // TODO:
+    return -1;
+    
+}
+
+
+/* Function: addWord
+ *
+ *    Adds the word into WordList (words have no spaces).  If WordList does not have
+ *    enough space to add word, addWord will resize with just enough space to allow for
+ *    the addition of word. If addWord needed to resize then return 1, otherwise if there
+ *    already enough space to add word without resizing, return 0. If word is empty
+ *    do nothing return -1. If m_list was nullptr, everything above still holds true except
+ *    return -2.
+ */
+int    WordList::addWord(const char word[]) {
+    
+    // TODO
+    return -2;
+    
+}
+
+
+/* Funtion: removeWord
+ *
+ *
+ *    If m_list is nullptr, returns -1.  Otherwise, searches for every
+ *    occurrence of word[], and removes that word of the list, returns
+ *    the number of words removed.
+ */
+int    WordList::removeWord(const char word[]) {
+    
+    // TODO:
+    return -1;
+}
+
+
+/* Funtion: appendList
+ *
+ *
+ *    Appends the contents of src_list to the WordList.  If WordList does
+ *    not have enough space appendList should dynamically allocate enough space
+ *    to append the contents of src_list to WordList, returns number of words
+ *    appended.  If src_list is nullptr or empty appendList does nothing and
+ *    returns -1. If this WordList::m_list is nullptr everything above still
+ *    holds but returns -2.
+ */
+int    WordList::appendLists(const WordList* src_list) {
+    
+    // TODO:
+    return -2;
+    
+}
+
+
+/* Funtion: findWord
+ *
+ *    Finds the first occurrence of the word in the WordList
+ *    returns the position of that word in the list.  Otherwise,
+ *    if the word is not in the WordList or if m_list is nullptr
+ *    return -1.
+ */
+int WordList::findWord(const char word[]) const {
+    
+    // TODO:
+    return -1;
+    
+}
+
+
+/* Funtion: sortList
+ *
+ *    If m_list is null return -1.  If there is only one word
+ *    in the list return 1.  Otherwise, sortList sorts the
+ *    words in WordList in ascending order.  Returns 0 on
+ *    success.
+ */
+int    WordList::sortList() {
+    
+    // TODO:
+    return -1;
+    
+}
+
+
+/* Funtion: Assignment Operator
+ *
+ *    Overload the assignment operator for WordList.  Makes a deep
+ *    copy from src_list to this WordList.
+ */
+WordList& WordList::operator=(const WordList &src_list) {
+    
+    // TODO:
+    return *this;
+}
