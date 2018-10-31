7248db55695de3f81885b9157fb8641e306cc458
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-10-31 11:12:20 -0700

    add tests for default constructor

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -1,7 +1,15 @@
-#include"list.h"
 #include"Studentinfo.h"
 #include<iostream>
+#include<assert.h>
 
+// Choose which test to compile
+#define TEST1
+
+#ifdef TEST0
+
+#include"list.h"
+
+// Sample Usage
 int main() {
     
     std::cout << StudentInfo::name() << std::endl;
@@ -19,3 +27,30 @@ int main() {
     
     return 0;
 }
+
+#elif defined TEST1
+
+#define MAKE_MEMBERS_PUBLIC
+#include"list.h"
+
+// Test default constructor
+int main() {
+    
+    List<int> l; //List of integers
+    assert(l.head == nullptr);
+    assert(l.tail == nullptr);
+    assert(l.size == 0);
+    
+    return 0;
+}
+
+#else
+
+#include"list.h"
+
+int main() {
+    
+    return 0;
+}
+
+#endif
